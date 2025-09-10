23f285468e1f2836ab15d859f614bb2c628568e4
Creed Production <creedproduction4pps@gmail.com>
2025-09-10 11:57:50 +0300

    Update downloaderRoutes.js

--- Routes/downloaderRoutes.js
+++ Routes/downloaderRoutes.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const downloaderController = require('../Controllers/downloaderController');
-const mockController = require('../Controllers/mockController')
+const { downloadMedia } = require('../Controllers/downloaderController');
+const mockController = require('../Controllers/mockController');
 
 // POST route to download media
-router.post('/download', downloaderController.downloadMedia);
+router.post('/download', downloadMedia);
 
 // GET route to fetch mock data
 router.get('/mock-videos', mockController.getMockVideos);
 
+// Test endpoint
+router.get('/test', (req, res) => {
+  res.status(200).json({
+    message: 'Download API is working',
+    timestamp: new Date().toISOString(),
+    supportedPlatforms: [
+      'instagram',
+      'tiktok',
+      'facebook',
+      'twitter',
+      'youtube',
+      'pinterest',
+      'threads',
+      'linkedin'
+    ]
+  });
+});
 
 module.exports = router;
